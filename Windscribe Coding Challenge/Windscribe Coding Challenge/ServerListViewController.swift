f79f5dc58186623b918b443c1250ee984c9f5899
Deep Desai <deep.hemant.desai@gmail.com>
2021-05-30 10:52:01 -0400

    Created project and configured code to remove storyboard

---
+++ Windscribe Coding Challenge/Windscribe Coding Challenge/ServerListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ServerListViewController.swift
+//  Windscribe Coding Challenge
+//
+//  Created by Deep on 2021-05-30.
+//
+
+import UIKit
+
+class ServerListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
